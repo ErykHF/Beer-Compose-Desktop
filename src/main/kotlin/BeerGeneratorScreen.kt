a81645050abcdc9b05aee02601ba2481189461aa
Eryk Ferenc <Cybersnake@hotmail.co.uk>
2022-02-18 19:57:13 +0000

    Adjust screen widgets/UI

--- src/main/kotlin/BeerGeneratorScreen.kt
+++ src/main/kotlin/BeerGeneratorScreen.kt
@@ -20,8 +20,9 @@ import utils.Util.loadNetworkImage
 fun BeerGeneratorScreen(beerInfo: BeerInfo = BeerInfo()) {
 
     val beerName by remember { beerInfo.beerName }
-    val beerDesc by remember { beerInfo.beerDesc }
     val beerFirstMade by remember { beerInfo.beerFirstMade }
+    val beerTagline by remember { beerInfo.beerTagline }
+    val beerDesc by remember { beerInfo.beerDesc }
     val beerImage by remember { beerInfo.beerImage }
     val refreshBeers = remember { beerInfo }
 
@@ -30,27 +31,41 @@ fun BeerGeneratorScreen(beerInfo: BeerInfo = BeerInfo()) {
             val imageModifier = Modifier
                 .height(240.dp)
                 .fillMaxWidth()
-                .clip(RoundedCornerShape(12.dp))
+                .padding(top = 16.dp)
             Image(
                 bitmap = loadNetworkImage(beerImage),
                 contentDescription = "image",
                 imageModifier,
                 contentScale = ContentScale.Fit
             )
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(text = beerName, modifier = Modifier.align(Alignment.CenterHorizontally).padding(6.dp))
+            Spacer(modifier = Modifier.height(12.dp))
             Text(
-                text = beerDesc,
-                modifier = Modifier.align(Alignment.CenterHorizontally).padding(6.dp),
+                text = beerName,
+                modifier = Modifier.align(Alignment.CenterHorizontally).padding(2.dp),
                 textAlign = TextAlign.Center
             )
-            Text(text = beerFirstMade, modifier = Modifier.align(Alignment.CenterHorizontally).padding(6.dp))
-            Spacer(modifier = Modifier.height(16.dp))
-        }
-        Button(onClick = {
-            refreshBeers.getTheBeers()
-        }, modifier = Modifier.align(Alignment.BottomCenter)) {
-            Text("Generate")
+            Text(
+                text = beerTagline,
+                modifier = Modifier.align(Alignment.CenterHorizontally).padding(2.dp),
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = beerFirstMade,
+                modifier = Modifier.align(Alignment.CenterHorizontally).padding(2.dp),
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = beerDesc,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+                    .padding(top = 2.dp, bottom = 20.dp, start = 16.dp, end = 16.dp),
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            Button(onClick = {
+                refreshBeers.getTheBeers()
+            }, modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                Text("Generate")
+            }
         }
     }
 }
--- src/main/kotlin/api/BeerInfo.kt
+++ src/main/kotlin/api/BeerInfo.kt
@@ -11,10 +11,10 @@ class BeerInfo {
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
     var beerName = mutableStateOf("")
-    var beerDesc = mutableStateOf("")
     var beerFirstMade = mutableStateOf("")
-    var beerImage =
-        mutableStateOf<String?>("https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png")
+    var beerDesc = mutableStateOf("")
+    var beerTagline = mutableStateOf("")
+    var beerImage = mutableStateOf<String?>("https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png")
 
     init {
         getTheBeers()
@@ -23,11 +23,14 @@ class BeerInfo {
     fun getTheBeers() {
         coroutineScope.launch {
             val result = RetrofitClient.retrofit.loadImages()
-            beerName.value = result.first().name
-            beerDesc.value = result.first().description
-            beerFirstMade.value = result.first().first_brewed
-            beerImage.value = result.first().image_url
-                ?: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png"
+            for (beerItem in result){
+                beerName.value = beerItem.name
+                beerFirstMade.value = beerItem.first_brewed
+                beerDesc.value = beerItem.description
+                beerTagline.value = beerItem.tagline
+                beerImage.value = beerItem.image_url
+                    ?: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png"
+            }
         }
     }
 }
